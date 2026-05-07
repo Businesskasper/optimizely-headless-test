1c3370307593e422dc91492e566986d81b0ccccd
Luka Weis <lukat.weis@gmail.com>
2026-05-07 23:49:36 +0200

    fix: TextBlockRenderer correctly expects value

--- frontend/src/components/TextBlockRenderer.tsx
+++ frontend/src/components/TextBlockRenderer.tsx
@@ -19,7 +19,7 @@ export function TextBlockRenderer({ block }: { block: TextBlock }) {
     <Paper p="md" withBorder>
       <Stack gap="sm">
         <Title order={2}>{block.heading.value}</Title>
-        <div dangerouslySetInnerHTML={{ __html: block.text?.value }} />
+        <div dangerouslySetInnerHTML={{ __html: block.text.value }} />
       </Stack>
     </Paper>
   );
